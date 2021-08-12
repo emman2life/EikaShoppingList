3b6a25c05a60288acb24616feac46de1f6904f7a
Emmanuel Imhontu <imhontu.emmanuel@yahoo.com>
2021-08-12 11:21:43 +0200

    illustration at the welcome page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import useFetch from './customHooks/useFetch';
 import { ListContext } from './ListContext';
 import FormDisplayReducer from './FormDisplayReducer';
 import SortList from './components/sort/SortList';
+import illustration from './assets/Illustration/undraw_empty_cart.svg'
 
 
 function App() {
@@ -19,10 +20,15 @@ function App() {
   const list = useFetch(sortBy);
 
   const welcomeText = (
+    <div>
+      <div className="illustration">
+        <img src={illustration} alt=""/>
+      </div>
     <p className="welcome-text">
       Welcome to EIKA, thank for using this application. To add item to your
       shopping list, click the button “Add item” below.
     </p>
+    </div>
   );
 
   const sortByHandler = (sortByString) => {
